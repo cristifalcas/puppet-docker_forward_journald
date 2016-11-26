bae7d84f4d465b280c3228bb15ebddca8a151a5d
Cristian Falcas <cristi.falcas@gmail.com>
2016-11-26 17:43:05 +0200

    spec fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,25 +3,21 @@
 #
 # Installs docker-forward-journald package.
 #
-# Parameters
-#----------
+# Parameters:
 #
-# * ensure_package
-# Version of package to be installed
+# $ensure_package::     Version of package to be installed
 #
-# * package_name
-# Name of the package to be installed
+# $package_name::       Name of the package to be installed
 #
-# * binary_chmod
-# By default the binary is set to mode 0700 (only root has access)
-# This can be changed with this parameter
+# $binary_chmod::       By default the binary is set to mode 0700 (only root has access)
+#                       This can be changed with this parameter
 #
 class forward_journald (
   $ensure_package = 'installed',
   $package_name   = 'docker-forward-journald',
   $binary_chmod   = '0700',
 ) {
-  if $::operatingsystemmajrelease == 7 {
+  if $::operatingsystemmajrelease == '7' {
     package { $package_name: ensure => $ensure_package, } ->
     file { '/usr/bin/forward-journald':
       ensure => file,
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe 'forward_journald', :type => :class do
+describe 'forward_journald' do
   on_supported_os.each do |os, facts|
-    context "on #{os}" do
+    context "with defaults on #{os}" do
       let(:facts) do
-        facts.merge({:puppetversion => Puppet.version})
+        facts.merge({:puppetmaster => 'localhost.localdomain'})
       end
-      it 'test default install' do
-        is_expected.to compile.with_all_deps
-      end
+      let(:params) { { :ensure_package => 'installed' } }
+      it { should compile.with_all_deps }
+      it { should contain_class('forward_journald') }
     end
   end
 end
